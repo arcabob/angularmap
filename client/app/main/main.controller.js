73bb151b31f6a958af6f8fff2bed037c8a9d4332
Bob Erlenbaugh <Bob.Erlenbaugh@theice.com>
2014-11-19 17:14:38 -0600

    updated watch

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,13 +3,48 @@
 angular.module('remapApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [];
-    $scope.map = { center: { latitude: 39.830033, longitude: -89.6400523 }, zoom: 7 ,mapTypeId: 'roadmap'};
-    $scope.showLayer = true;
-    $scope.layerOptions = {
-      query: {
-      select: 'geometry',
-        from: '1OoBTpAqkASRRnJ_tsUOiswz06-0r2Nc9ncxQM68',
-        where: "'State' = 'IL'"}};
+    //$scope.map = { center: { latitude: 39.830033, longitude: -89.6400523 }, zoom: 7 ,mapTypeId: 'roadmap'};
+
+    $scope.currentState = 'IL';
+
+    $scope.map = {
+      center: {latitude: 39.830033, longitude: -89.6400523 },
+      zoom: 7,
+      layer: {
+        show: true,
+        options: {
+          query: {
+            select: 'geometry',
+            from: '1OoBTpAqkASRRnJ_tsUOiswz06-0r2Nc9ncxQM68',
+            where: "'State' = 'IL'"
+          },
+          styles: [{
+            polygonOptions: {
+              fillColor: '#00FF00',
+              fillOpacity: 0.35
+            }}]
+        }
+      }
+    };
+
+
+/*
+ ,
+ styles: [{
+ polygonOptions: {
+ fillColor: '#00FF00',
+ fillOpacity: 0.35
+ }}]
+ */
+
+
+
+    $scope.$watch('currentState', function() {
+      $scope.map.layer.options.query = {
+        select: "geometry",
+        from: "1OoBTpAqkASRRnJ_tsUOiswz06-0r2Nc9ncxQM68",
+        where: "'State' = '" + $scope.currentState + "'"};
+    });
 
 
     /*$http.get('/api/things').success(function(awesomeThings) {
