0c4cca56725968bddd78d49c1d370926b0901164
Bob Erlenbaugh <Bob.Erlenbaugh@theice.com>
2014-11-19 12:59:04 -0600

    added FusionTablesLayer

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,7 +3,14 @@
 angular.module('remapApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [];
-    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+    $scope.map = { center: { latitude: 39.830033, longitude: -89.6400523 }, zoom: 7 ,mapTypeId: 'roadmap'};
+    $scope.showLayer = true;
+    $scope.layerOptions = {
+      query: {
+      select: 'geometry',
+        from: '1OoBTpAqkASRRnJ_tsUOiswz06-0r2Nc9ncxQM68',
+        where: "'State' = 'IL'"}};
+
 
     /*$http.get('/api/things').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
